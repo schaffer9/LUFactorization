8c88b24de1e9292e57c34a792a298c023972574a
Sebastian Schaffer <26623931+sschaffer92@users.noreply.github.com>
2019-04-27 21:20:52 +0200

    Update Readme

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
 This repository implements a LU solver for dense linear systems.
 
 # Usage
-Make sure that you have julia installed and that the following packages are installed:
-- CSV
-- DataFrames
-
-You can install the packages as follows:
+To install the project, first install *Julia*. After you did so you can add the project as follows.
+Open a new shell and type
 ```
     $ julia
     julia> using Pkg
-    julia> Pkg.add("CSV")
-    julia> Pkg.add("DataFrames")
+    julia> Pkg.add("https://github.com/sschaffer92/LUFactorization")
+
 ```
 
-After doing this you can run the code with ``julia main.jl``. This will generate three *csv* files which provide
-runtime and accuracy data for *LU factorization*, *forward substitution* and *backward substitution*.
+After doing so you can download the `main.jl` file of the package and
+call `./main.jl --help`
 
+
+```
+usage: main.jl [-t THREADS] [-s SIZE] [-b] [-g BLOCK-SIZE] [-p]
+               [-r RUNS] [-h] path
+
+positional arguments:
+  path                  The csv file path where the result is stored
+
+optional arguments:
+  -t, --threads THREADS
+                        Number of BLAS threads (type: Int64, default:
+                        5)
+  -s, --size SIZE       The maximum system size to solve (min 100)
+                        (default: "3000")
+  -b, --blocked         Run the blocked version of the algorithm
+  -g, --block-size BLOCK-SIZE
+                        The block size if the factorization is blocked
+                        (type: Int64, default: 100)
+  -p, --no-pivoting     Do not apply pivoting
+  -r, --runs RUNS       The number of runs to calculate the average
+                        time and accuracy (type: Int64, default: 5)
+  -h, --help            show this help message and exit
+```
--- main.jl
+++ main.jl
@@ -1,43 +1,46 @@
 #!/usr/bin/env julia
 
+
+using ArgParse
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "path"
+        help = "The csv file path where the result is stored"
+        required = true
+    "--threads", "-t"
+        help = "Number of BLAS threads"
+        arg_type = Int
+        default = 5
+    "--size", "-s"
+        help = "The maximum system size to solve (min 100)"
+        arg_type = String
+        default = "3000"
+    "--blocked", "-b"
+        help = "Run the blocked version of the algorithm"
+        action = :store_true
+    "--block-size", "-g"
+        help = "The block size if the factorization is blocked"
+        arg_type = Int
+        default = 100
+    "--no-pivoting", "-p"
+        help = "Do not apply pivoting"
+        action = :store_false
+    "--runs", "-r"
+        help = "The number of runs to calculate the average time and accuracy"
+        arg_type = Int
+        default = 5
+end
+PARSED_ARGS = parse_args(ARGS, s)
+
+
 using LUFactorization
 using LinearAlgebra
 using CSV
-using ArgParse
 
 
+function main(parsed_args)
 
-function main(args)
-
-    s = ArgParseSettings()
-    @add_arg_table s begin
-        "path"
-            help = "The csv file path where the result is stored"
-            required = true
-        "--threads", "-t"
-            help = "Number of BLAS threads"
-            arg_type = Int
-            default = 5
-        "--size", "-s"
-            help = "The maximum system size to solve (min 100)"
-            arg_type = String
-            default = "3000"
-        "--blocked", "-b"
-            help = "Run the blocked version of the algorithm"
-            action = :store_true
-        "--block-size", "-g"
-            help = "The block size if the factorization is blocked"
-            arg_type = Int
-            default = 100
-        "--no-pivoting", "-p"
-            help = "Do not apply pivoting"
-            action = :store_false
-        "--runs", "-r"
-            help = "The number of runs to calculate the average time and accuracy"
-            arg_type = Int
-            default = 5
-    end
-    parsed_args = parse_args(args, s)
     size = eval(Meta.parse(parsed_args["size"]))
     directory_path = dirname(parsed_args["path"])
     mkpath(directory_path)
@@ -55,5 +58,5 @@ function main(args)
 end
 
 if  !isinteractive()
-    main(ARGS)
+    main(PARSED_ARGS)
 end
